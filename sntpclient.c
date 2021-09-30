ddd184a27d9f64e87ea9b4474d920068f79893ba
alexandre <alex14fr@gmail.com>
2021-09-30 20:21:56 +0200

    x

--- sntpclient.c
+++ sntpclient.c
@@ -42,8 +42,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 int main(int argc, char **argv) {
 	int sock=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-	char buf[49];
-	bzero(&buf,49);
+	char buf[48];
+	bzero(&buf,48);
 	buf[0]=(1<<3)|3;
 	struct hostent *host;
 	host=gethostbyname((argc>1 ? argv[1]:"pool.ntp.org"));
@@ -57,6 +57,6 @@ int main(int argc, char **argv) {
 	read(sock,buf,48);
 	if(getenv("DEBUG")) write(1,buf,48);
 	int32_t tm=(buf[32]<<24 | buf[33]<<16 | buf[34]<<8 | buf[35])-2208988800;
-	// (  date -u -d "1900-01-01 00:00" +%s -> -2208988800 )
+	// (  date -u -d "1900-01-01 00:00" +%s == -2208988800 )
 	printf("%d\n",tm);
 }
