c2eb2502cfcfa352f828d29b25a05ce866ae74c8
alexandre <alex14fr@gmail.com>
2021-09-30 20:26:14 +0200

    x

--- sntpclient.c
+++ sntpclient.c
@@ -47,14 +47,27 @@ int main(int argc, char **argv) {
 	buf[0]=(1<<3)|3;
 	struct hostent *host;
 	host=gethostbyname((argc>1 ? argv[1]:"pool.ntp.org"));
+	if(!host) {
+		perror("gethostbyname");
+		exit(1);
+	}
 	struct sockaddr_in saddr;
 	bzero(&saddr, sizeof(struct sockaddr_in));
 	saddr.sin_family=AF_INET;
 	saddr.sin_port=htons(123);
 	saddr.sin_addr.s_addr=*(long *)(host->h_addr_list[0]);
-	connect(sock,(struct sockaddr *)&saddr,sizeof(struct sockaddr_in));
-	write(sock,buf,48);
-	read(sock,buf,48);
+	if(connect(sock,(struct sockaddr *)&saddr,sizeof(struct sockaddr_in))!=0) {
+		perror("connect");
+		exit(1);
+	}
+	if(write(sock,buf,48)<=0) {
+		perror("write");
+		exit(1);
+	}
+	if(read(sock,buf,48)<=0) {
+		perror("read");
+		exit(1);
+	}
 	if(getenv("DEBUG")) write(1,buf,48);
 	int32_t tm=(buf[32]<<24 | buf[33]<<16 | buf[34]<<8 | buf[35])-2208988800;
 	// (  date -u -d "1900-01-01 00:00" +%s == -2208988800 )
